50c86bfa5082074dcc86b0ab9a3f3e9e5b6e76e9
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-07-31 16:17:51 -0300

    hicimos unas pruebas con ema para analizar como seria el acceso a los datos de los usuarios.

--- docker/src/sileg/model/SilegModel.py
+++ docker/src/sileg/model/SilegModel.py
@@ -19,39 +19,51 @@ engine = create_engine('postgresql://{}:{}@{}:5432/{}'.format(
 
 Session = sessionmaker(bind=engine)
 
+class UsuarioDatos:
+
+    def __init__(self):
+        self.nombre
+        self.apellido
+        self.correoInstitucional
+        self.correoAlternativo
 
 
 class SilegModel:
 
+    @classmethod
+    def obtenerUsuarios(cls):
 
-    
+        s = Session()
+        s.query(Usuarios)
+
+
     @classmethod
     def designaciones(cls, render):
         """ consulta de designaciones con sus respectivas relaciones utilizando filtros opcionales """
-        
-        session = Session()        
-       
+
+        session = Session()
+
         designaciones = Designacion.find(session, render)
 
-        idsUsuarios = [d.usuario.id for d in designaciones]           
+        idsUsuarios = [d.usuario.id for d in designaciones]
         usuarios_ = UsuariosModel.usuariosByIds(idsUsuarios)
 
         usuarios = {}
         for u in usuarios_:
             usuarios[u["id"]] = u
-          
-  
+
+
         response = []
         for d in designaciones:
             u = usuarios[d.usuario.id]
-    
+
             if d.lugar.tipo == "catedra":
                 detalle = {"id":d.lugar.materia.id, "nombre":d.lugar.materia.nombre}
-            else:               
+            else:
                 detalle = {"id":d.lugar.padre.id, "nombre":d.lugar.padre.nombre} if d.lugar.padre else None
 
             r = {
-              "usuario":{"id":u["id"], "nombres":u["name"], "apellidos":u["lastname"], "numero_documento":u["dni"]},            
+              "usuario":{"id":u["id"], "nombres":u["name"], "apellidos":u["lastname"], "numero_documento":u["dni"]},
               "lugar":{"id":d.lugar.id, "nombre":d.lugar.nombre, "detalle":detalle},
               "cargo":{"id":d.cargo.id, "nombre":d.cargo.nombre},
               "desde":d.desde,
@@ -62,8 +74,5 @@ class SilegModel:
               "tipo":d.tipo
             }
             response.append(r)
-              
+
         return response
-                    
-
-
--- docker/src/sileg/model/entities/Usuario.py
+++ docker/src/sileg/model/entities/Usuario.py
@@ -2,9 +2,15 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_utils import Base
 
-
+import json
+import requests
 
 class Usuario(Base):
 
     __tablename__ = 'usuario'
     __table_args__ = {'schema':'sileg'}
+
+    def resolveUser(self):
+        ''' se hace la llamada rest a la api de usuarios '''
+        data = requests.get('http://usuarios.econo.unlp.edu.ar/api/v1.0/users/{}'.format(self.id)).json()
+        self.__dict__ = json.loads(data)
