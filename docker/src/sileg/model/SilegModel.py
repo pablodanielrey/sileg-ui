53af73f7d7792207f0296af952293af86ad8ee7b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-01 14:11:56 -0300

    movi el armado de la session del sqlalchemy a model del sileg. no especificamente de un modelo solo

--- docker/src/sileg/model/SilegModel.py
+++ docker/src/sileg/model/SilegModel.py
@@ -1,23 +1,9 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.schema import CreateSchema
-from sqlalchemy.orm import sessionmaker, joinedload
-
-from model_utils import Base
-
-from sileg.model.entities import *
-
-from sileg.model.UsuariosModel import UsuariosModel
-
-engine = create_engine('postgresql://{}:{}@{}:5432/{}'.format(
-    os.environ['SILEG_DB_USER'],
-    os.environ['SILEG_DB_PASSWORD'],
-    os.environ['SILEG_DB_HOST'],
-    os.environ['SILEG_DB_NAME']
-), echo=True)
+from sqlalchemy.orm import joinedload
+from .entities import *
+from . import Session
+from .UsuariosModel import UsuariosModel
 
 
-Session = sessionmaker(bind=engine)
 
 class UsuarioDatos:
 
@@ -29,10 +15,6 @@ class UsuarioDatos:
 
 
 class SilegModel:
-
-    @staticmethod
-    def onlyKeys(d, keys):
-        return {k: d[k] for k in keys}
 
     @classmethod
     def obtenerUsuarios(cls):
--- docker/src/sileg/model/__init__.py
+++ docker/src/sileg/model/__init__.py
@@ -1,3 +1,19 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.schema import CreateSchema
+from sqlalchemy.orm import sessionmaker
+
+from model_utils import Base
+
+engine = create_engine('postgresql://{}:{}@{}:5432/{}'.format(
+    os.environ['SILEG_DB_USER'],
+    os.environ['SILEG_DB_PASSWORD'],
+    os.environ['SILEG_DB_HOST'],
+    os.environ['SILEG_DB_NAME']
+), echo=True)
+Session = sessionmaker(bind=engine)
+
+
 
 from .SilegModel import SilegModel
 
