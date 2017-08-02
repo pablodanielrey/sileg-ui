4229723560afeed568b3feadbce823fdc9f7b18b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-02 10:02:38 -0300

    elimine codigo al pedo que había agregado ivan

--- docker/src/libs/model_utils/__init__.py
+++ docker/src/libs/model_utils/__init__.py
@@ -19,24 +19,4 @@ class MyBaseClass:
     def findAll(cls, s):
         return s.query(cls).all()
 
-
-    @classmethod
-    def render(cls, render):
-        """ Redefinir clase de presentacion """
-
-        ret = {
-          "size": render["size"] if "size" in render and int(render["size"]) else None,
-          "page": render["page"] if "page" in render and int(render["page"]) - 1 else 0,
-          "order": render["order"] if "order" in render and render["order"] else [],
-          "filters": render["filters"] if "filters" in render and render["filters"] else []
-        }
-
-        #params son filters con opcion "=="
-        if "params" in render and render["params"]:
-            for field, value in render["params"].items():
-                ret["filters"].append({"id":field, "option":"==", "value":value})
-
-        return ret
-
 Base = declarative_base(cls=(JsonSerializableBase,MyBaseClass))
-#Base = declarative_base(cls=(MyBaseClass,))
