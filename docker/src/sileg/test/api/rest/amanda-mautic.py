e9f1b862ac2f307b0bfd2271f971409d14515d41
31381082 <emanuel@econo.unlp.edu.ar>
2017-08-23 18:47:08 -0300

    Ya me pude conectar a la api del mautic

--- docker/src/sileg/test/api/rest/amanda-mautic.py
+++ docker/src/sileg/test/api/rest/amanda-mautic.py
@@ -1,10 +1,15 @@
 
 import requests
+from requests.auth import HTTPBasicAuth
 import os
 
-sileg_api = 'http://127.0.0.1:5001/sileg/api/v1.0'
-#usuarios_api = os.environ['USER_REST_URL']
-usuarios_api = 'http://163.10.56.55:7001/users/api/v1.0'
+
+usuarios_api = os.environ['USER_REST_URL']
+sileg_api = os.environ['SILEG_REST_URL']
+usuario_mautic = os.environ['MAUTIC_USER']
+clave_mautic = os.environ['MAUTIC_PASSWORD']
+mautic_api = os.environ['MAUTIC_REST_URL']
+
 
 def getEconoMail(usuario):
     if len(usuario['mails']) <= 0:
@@ -15,6 +20,25 @@ def getEconoMail(usuario):
     return None
 
 if __name__ == '__main__':
+
+    # mautic
+
+    # obtengo los segmentos
+    segmentos = requests.get(mautic_api + 'segments/', auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
+
+    for s in segmentos["lists"].values():
+        print(s)
+        break;
+
+    # obtengo los miembros de un segmento
+    users = requests.get(mautic_api + 'contacts/?search=segment:rebotados', auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
+    for u in users["contacts"].values():
+        print('id: {}'.format(u["id"]))
+
+    exit()
+
+
+
     # obtengo los cargos
     cargosA = requests.get('http://127.0.0.1:5001/sileg/api/v1.0/cargos/').json()
     cargos = {}
@@ -24,8 +48,10 @@ if __name__ == '__main__':
 
     result = {}
     desig = requests.get(sileg_api + '/designaciones').json()
-    print(len(desig))
+    i = 0
     for d in desig:
+        i = i + 1
+        print("procesando desginacion {} de {}".format(i, len(desig)))
         m = {'nombre':'-'}
         cid = d['lugar_id']
         cs = requests.get(sileg_api + '/catedras/' + cid).json()
@@ -51,80 +77,8 @@ if __name__ == '__main__':
         if id in result:
             r = result[id]
             r["usuarios"].append(user)
-            print("Creando usuario {}, {}".format(user["nombre"], user["apellido"]))
         else:
             result[id] = {'nombre': nombre, 'usuarios':[user]}
-            print("Agregando usuario {}, {}".format(user["nombre"], user["apellido"]))
 
     for value in result.values():
         print('Nombre: {} -- usuarios: {}'.format(value["nombre"], value["usuarios"]))
-    exit()
-
-    '''
-
-    deptos = requests.get('http://127.0.0.1:5001/sileg/api/v1.0/departamentos').json()
-    for d in deptos:
-        print('------------ {} -------------'.format(d['nombre']))
-        did = d['id']
-        materias = requests.get('http://127.0.0.1:5001/sileg/api/v1.0/materias/?d={}'.format(did)).json()
-        for m in materias:
-            print(m)
-    exit()
-    '''
-    """
-    deptos = requests.get('http://127.0.0.1:5001/sileg/api/v1.0/departamentos').json()
-    for d in deptos:
-        did = d['id']
-        materias = requests.get('http://127.0.0.1:5001/sileg/api/v1.0/materias/?d={}'.format(did)).json()
-        for m in materias:
-            print('''
-Departamento: {}
-Materia: {}
-Cátedra: {}
-            '''.format(d['nombre'], m['materia']['nombre'], m['nombre']))
-    """
-
-    with open('/tmp/sileg.csv','w') as f:
-
-        desig = requests.get(sileg_api + '/designaciones').json()
-        for d in desig:
-
-            m = {'nombre':'-'}
-            cid = d['lugar_id']
-            cs = requests.get(sileg_api + '/catedras/' + cid).json()
-            if len(cs) <= 0:
-                ''' no es una catedra '''
-                continue
-
-            c = cs[0]
-            cargo = cargos[d['cargo_id']]
-            uid = d['usuario_id']
-            u = requests.get(usuarios_api + '/usuarios/' + uid).json()[0]
-
-            datos = {
-                'departamento': c['padre']['nombre'],
-                'materia': c['materia']['nombre'],
-                'catedra': c['nombre'],
-                'persona': u['dni'] + ' ' + u['nombre'] + ' ' + u['apellido'],
-                'email': getEconoMail(u),
-                'cargo': cargo['nombre'] + ' ' + cargo['tipo']
-            }
-
-
-            f.write(datos['departamento'] + ',' + datos['materia'] + ',' + datos['catedra'] + ',' + datos['persona'] + ',' + datos['email'] + ',' + datos['cargo'] + '\n')
-
-            print('''
-    Departamento: {}
-    Materia: {}
-    Catedra: {}
-    Persona: {}
-    Email: {}
-    Cargo: {}
-            '''.format(
-                datos['departamento'],
-                datos['materia'],
-                datos['catedra'],
-                datos['persona'],
-                datos['email'],
-                datos['cargo']
-            ))
