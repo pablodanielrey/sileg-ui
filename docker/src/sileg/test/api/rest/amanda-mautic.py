7fdb9e987b95ccd840738cbd5a47872b01b46726
31381082 <emanuel@econo.unlp.edu.ar>
2017-08-24 12:14:45 -0300

    Quedo funcionando la creacion de listas del mautic

--- docker/src/sileg/test/api/rest/amanda-mautic.py
+++ docker/src/sileg/test/api/rest/amanda-mautic.py
@@ -20,12 +20,26 @@ def getEconoMail(usuario):
     return None
 
 def obtenerSegmentos():
-    segmentos = requests.get(mautic_api + 'segments/', auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
-    return segmentos["lists"].values()
+    segmentos = requests.get(mautic_api + 'segments/?start=0&limit=100', auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
+    total = segmentos["total"]
+    limit = int(total / 100)
+    retorno = list(segmentos["lists"].values())
+    for i in range(0, limit):
+        start = (i + 1) * 100
+        url = "{}segments/?start={}&limit=100".format(mautic_api, start)
+        r = requests.get(url, auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
+        retorno.extend(list(r["lists"].values()))
+
+    return retorno
 
 def obtenerMiembrosSegmento(alias_segmento):
     users = requests.get(mautic_api + 'contacts/?search=segment:' + alias_segmento, auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
-    return users["contacts"].values()
+    return users["contacts"].values() if len(users["contacts"]) > 0 else []
+
+def agregarMiembro(uid, sid):
+    url = "{}segments/{}/contact/add/{}".format(mautic_api, sid, uid)
+    print(url)
+    return requests.post(url, auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
 
 def parseUser(u):
     fields = u["fields"]
@@ -49,27 +63,33 @@ segmento:
 def crearSegmento(segmento):
     return requests.post(mautic_api + 'segments/new', auth=HTTPBasicAuth(usuario_mautic, clave_mautic), data=segmento).json()
 
+# usuario = {'nombre':"", "apellido":"", "email": ""}
+def crearUsuario(usuario):
+    data = {'firstname':usuario["nombre"], 'lastname':usuario["apellido"], "email":usuario["email"]}
+    return requests.post(mautic_api + 'contacts/new', auth=HTTPBasicAuth(usuario_mautic, clave_mautic), data=data).json()
+
 def normalizarAlias(alias):
-    return alias.lower().replace(".","-").replace(" ","").replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u").replace("ñ","n")
+    return alias.lower().replace(".","-").replace(" ","").replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u").replace("ñ","n").replace("(","").replace(")","")
+
+# user: {'nombre':"", "apellido":"", "email": ""}
+# members: arreglo de usuarios de mautic
+def esMiembro(user, members):
+    for m in members:
+        u = parseUser(m)
+        if user["email"] == u["email"]:
+            return True
+    return False
+
+def obtenerUsuario(u):
+    users = requests.get(mautic_api + 'contacts/?search=email:' + u["email"], auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
+    return users["contacts"]
 
 if __name__ == '__main__':
-
-    # mautic
 
     # obtengo los segmentos
     segmentos = {}
     for s in obtenerSegmentos():
-        segmentos[s["alias"]] = {"name":s["name"], "alias":s["alias"]}
-    print(segmentos)
-
-    # obtengo los miembros de un segmento
-    users = obtenerMiembrosSegmento('rebotados')
-    for u in users:
-        print('Usuario: {}'.format(parseUser(u)))
-        break;
-        print('id: {}'.format(u["id"]))
-
-
+        segmentos[s["alias"]] = {"id":s["id"],"name":s["name"], "alias":s["alias"]}
 
     # obtengo los cargos
     cargosA = requests.get(sileg_api + '/cargos').json()
@@ -112,17 +132,35 @@ if __name__ == '__main__':
         else:
             result[id] = {'nombre': nombre, 'usuarios':[user]}
 
-        break; # lo puse para hacer mas rapido las pruebas, eliminarlo en producccion
 
     for value in result.values():
         seg = {"name":value["nombre"], "alias":normalizarAlias(value["nombre"])}
 
+        segmento = {}
+        print(seg["alias"])
         if seg["alias"] in segmentos:
             print("ya existe el segmento: " + seg["alias"])
-            continue
-        print("creando segmento" + seg["alias"])
-        print(crearSegmento(seg))
+            segmento = segmentos[seg["alias"]]
+        else:
+            print("creando segmento " + seg["alias"])
+            segmento = crearSegmento(seg)["list"]
 
         # debo agregar los miembros al segmento
-        print('Nombre: {} -- usuarios: {}'.format(value["nombre"], value["usuarios"]))
-        break; # lo puse para hacer mas rapido las pruebas, eliminarlo en producccion
+        miembros = obtenerMiembrosSegmento(seg['alias'])
+        for u in value["usuarios"]:
+            if esMiembro(u, miembros):
+                continue
+            print("agregando miembro {}".format(u))
+            usuario = obtenerUsuario(u)
+            uid = None
+            if len(usuario) < 1:
+                print("Crear usuario: {}".format(u))
+                nuevo = crearUsuario(u)
+                print(nuevo)
+                uid = nuevo["contact"]["id"]
+            else:
+                ids = list(usuario.keys())
+                uid = ids[0]
+            print("UID {}".format(uid))
+
+            agregarMiembro(uid, segmento["id"])
