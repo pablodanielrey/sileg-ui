29b47c85e13b0a2fe2850bea2bffff87951e1b10
31381082 <emanuel@econo.unlp.edu.ar>
2017-08-28 08:24:54 -0300

    Quedo funcionando la consulta de los correos a la hoja de calculo de google

--- docker/src/sileg/test/api/rest/verificar-contactos.py
+++ docker/src/sileg/test/api/rest/verificar-contactos.py
@@ -2,30 +2,36 @@ from sileg.model.entities import Lugar
 from sileg.model.GoogleAuthApi import GAuthApis
 
 
+def obtenerCorreos(service, spreadsheetId,sheet):
+    range = sheet + "!A:A"
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range).execute()
+    values = result.get('values', [])
+    emails = []
+    if values:
+        for row in values:
+            emails.append(row)
+
+    print("La hoja {} tiene {} emails".format(sheet, len(emails)))
+    return emails
+
 if __name__ == '__main__':
 
     service = GAuthApis.getServiceSheet()
 
-    spreadsheetId = '' #id de la hoja
+    spreadsheetId = '1doSTh4R_g2RBc26lXpv-xaqLMhBH1ZtR2qHu8JwvGOM'
+    # spreadsheetId = '1URLHQ5jjMt1ladZatVllth5mWXfxgabNHlmS9El44UE'
     ranges = []
     include_grid_data = False
+
     # result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range='A1-A100').execute()
     result = service.spreadsheets().get(spreadsheetId=spreadsheetId).execute()
     sheets = result["sheets"]
+
+    emails = []
     for s in sheets:
-        print(s)
-        if "basicFilter" in s:
-            print(s["basicFilter"])
+        prop = s["properties"]
+        sheet = prop["title"]
+        print(sheet)
+        emails.extend(obtenerCorreos(service, spreadsheetId, sheet))
 
-
-    # range = {'sheetId': 145432231, 'startRowIndex': 0, 'endRowIndex': 552}
-    rangeName = 'A1:A100'
-    result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print(row)
+    print(len(emails))
