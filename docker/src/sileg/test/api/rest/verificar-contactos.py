d3ac6ebc385c0a5e27e56d0b3a1b17d7496fee02
31381082 <emanuel@econo.unlp.edu.ar>
2017-08-28 09:34:17 -0300

    Ya verifica los correos que existen en el mautic

--- docker/src/sileg/test/api/rest/verificar-contactos.py
+++ docker/src/sileg/test/api/rest/verificar-contactos.py
@@ -1,20 +1,37 @@
 from sileg.model.entities import Lugar
 from sileg.model.GoogleAuthApi import GAuthApis
+import requests
+from requests.auth import HTTPBasicAuth
 import os
 
+
+usuario_mautic = os.environ['MAUTIC_USER']
+clave_mautic = os.environ['MAUTIC_PASSWORD']
+mautic_api = os.environ['MAUTIC_REST_URL']
+
+def esCorreo(email):
+    r = email.strip().split('@')
+    return len(r) > 1 and r[1] != ''
+
+def obtenerUsuario(email):
+    users = requests.get(mautic_api + 'contacts/?search=email:' + email, auth=HTTPBasicAuth(usuario_mautic, clave_mautic)).json()
+    return users["contacts"]
+
 def obtenerCorreos(service, spreadsheetId,sheet):
-    range = sheet + "!A:A"
+    range = sheet + "!A2:A"
     result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range).execute()
     values = result.get('values', [])
-    emails = []
+    emails = set()
     if values:
         for row in values:
-            emails.append(row)
+            if len(row) > 0 and esCorreo(row[0]):
+                emails.add(row[0].strip())
 
     print("La hoja {} tiene {} emails".format(sheet, len(emails)))
     return emails
 
 if __name__ == '__main__':
+
 
     service = GAuthApis.getServiceSheet()
 
@@ -25,11 +42,22 @@ if __name__ == '__main__':
     result = service.spreadsheets().get(spreadsheetId=spreadsheetId).execute()
     sheets = result["sheets"]
 
-    emails = []
+    emails = set()
     for s in sheets:
         prop = s["properties"]
         sheet = prop["title"]
         print(sheet)
-        emails.extend(obtenerCorreos(service, spreadsheetId, sheet))
+        emails.update(obtenerCorreos(service, spreadsheetId, sheet))
 
-    print(len(emails))
+    existe = []
+    noExiste = []
+    for e in emails:
+        u = obtenerUsuario(e)
+        if len(u) > 0:
+            print("{} se encuentra en mautic".format(e))
+            existe.append(e)
+        else:
+            print("{} no existe en mautic".format(e))
+            noExiste.append(e)
+
+    print("Existen: {} No existen: {}".format(len(existe), len(noExiste)))
