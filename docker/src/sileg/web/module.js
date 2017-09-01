357ac940ce919c096e9d612f4b7b2014956c2471
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-09-01 10:06:09 -0300

    hago commit porque me voy a la facu

--- docker/src/sileg/web/module.js
+++ docker/src/sileg/web/module.js
@@ -3,6 +3,7 @@ app = angular.module('MainApp', ['ngRoute', 'ngResource'])
 app.config(['$routeProvider', function($routeProvider) {
 
   $routeProvider
+    .when('/designaciones', {templateUrl: '/componentes/designaciones/index.html', controller:'DesignacionesCtrl'})
     .when('/preload', {templateUrl: '/componentes/preload/index.html', controller:'PreloadCtrl'})
     .otherwise({ redirectTo: '/preload' });
 
