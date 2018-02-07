62aafd79d493896164159193cf330f31b07217a7
Emanuel Pais <emanuel@econo.unlp.edu.ar>
2018-02-07 09:52:47 -0300

    Camie el puerto del rest, estaba en el 5001

--- src/sileg/api/rest/main.py
+++ src/sileg/api/rest/main.py
@@ -104,7 +104,7 @@ def add_header(r):
     return r
 
 def main():
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == '__main__':
     main()
