b1ea934a53226341ce2cb1513c1ee626e7c734a8
Walter Blanco <walter.blanco@econo.unlp.edu.ar>
2018-03-21 10:47:20 -0300

    Estoy agregando una tabla de material a la pantalla de usuarios por oficina

--- src/sileg/src/app/lugares/usuarios-por-oficina/usuarios-por-oficina.component.ts
+++ src/sileg/src/app/lugares/usuarios-por-oficina/usuarios-por-oficina.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosPorOficinaComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
+
+export class TableBasicExample {
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+}
+
+export interface Element {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+];
