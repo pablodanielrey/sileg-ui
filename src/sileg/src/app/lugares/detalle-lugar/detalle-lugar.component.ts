43ea4832dbbdda5598554b52bda800818b78a3b7
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-03-21 12:02:03 -0300

    agregamos los tipos asi no tira error en la compilacion

--- src/sileg/src/app/lugares/detalle-lugar/detalle-lugar.component.ts
+++ src/sileg/src/app/lugares/detalle-lugar/detalle-lugar.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tipo {
+  nombre: string;
+  id: string;
+}
+
+
 @Component({
   selector: 'app-detalle-lugar',
   templateUrl: './detalle-lugar.component.html',
   styleUrls: ['./detalle-lugar.component.css']
 })
 export class DetalleLugarComponent implements OnInit {
+
+  tipos: Tipo[] = [];
 
   constructor() { }
 
