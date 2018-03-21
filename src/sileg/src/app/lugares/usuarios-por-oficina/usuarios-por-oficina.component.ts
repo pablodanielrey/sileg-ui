7470823b8a25635f37817987d4afcd93c5b16223
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-03-21 11:40:22 -0300

    agregamos una tabla de ejemplo

--- src/sileg/src/app/lugares/usuarios-por-oficina/usuarios-por-oficina.component.ts
+++ src/sileg/src/app/lugares/usuarios-por-oficina/usuarios-por-oficina.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface Usuario {
+  nombre: string;
+  apellido: string;
+  dni: string;
+  cargo: string;
+  fecha: Date;
+}
+
+export interface Cargo {
+  nombre: string;
+  id: string;
+}
+
+
 @Component({
   selector: 'app-usuarios-por-oficina',
   templateUrl: './usuarios-por-oficina.component.html',
@@ -7,29 +22,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosPorOficinaComponent implements OnInit {
 
+  usuarios: Usuario[] = [];
+  cargos: Cargo[] = [];
+  columnas: string[] = ['nombre','apellido','dni','cargo','fecha'];
 
   constructor() { }
 
   ngOnInit() {
+    this.usuarios = [
+      { nombre:'Walter', apellido:'Blanco', dni:'2726345', cargo:'1', fecha:new Date() },
+      { nombre:'Walter', apellido:'Blanco', dni:'2726345', cargo:'8', fecha:new Date() },
+      { nombre:'Walter', apellido:'Blanco', dni:'2726345', cargo:'7', fecha:new Date() },
+      { nombre:'Walter', apellido:'Blanco', dni:'2726345', cargo:'8', fecha:new Date() }
+    ];
+    this.cargos = [
+      { id: '1', nombre:'Cumple Función' },
+      { id: '2', nombre:'E7' },
+      { id: '3', nombre:'E6' },
+      { id: '4', nombre:'E5' },
+      { id: '5', nombre:'E4' },
+      { id: '6', nombre:'A2' },
+      { id: '7', nombre:'Contrato' },
+      { id: '8', nombre:'Beca' }
+    ];
+  }
 
+  cargoCambiado(event:any) {
+    console.log('cambiaaaaaadooooo');
   }
 
 }
-
-export class TableBasicExample {
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-}
-
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-];
