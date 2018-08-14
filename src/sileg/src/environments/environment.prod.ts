d2033b91a1c40ff369bb382293fda60bb3eb7aa2
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-08-14 17:14:55 -0300

    cambie el environment para econo

--- src/sileg/src/environments/environment.prod.ts
+++ src/sileg/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  silegApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/sileg/api/v1.0',
-  oidp_issuer: 'https://oidp.asistencia.presi.unlp.edu.ar',
-  userinfoEndpoint: 'https://oidp.asistencia.presi.unlp.edu.ar/userinfo',
-  loginUrl: 'https://oidp.asistencia.presi.unlp.edu.ar/oauth2/auth',
-  logoutUrl: 'https://consent.asistencia.presi.unlp.edu.ar/logout'
+  silegApiUrl: 'https://api.econo.unlp.edu.ar/sileg/api/v1.0',
+  oidp_issuer: 'https://oidp.econo.unlp.edu.ar',
+  userinfoEndpoint: 'https://oidp.econo.unlp.edu.ar/userinfo',
+  loginUrl: 'https://oidp.econo.unlp.edu.ar/oauth2/auth',
+  logoutUrl: 'https://consent.econo.unlp.edu.ar/logout'
   // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'  
 };
