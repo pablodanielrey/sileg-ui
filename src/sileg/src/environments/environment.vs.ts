04a2e544eb9eb4750dbf4d1e347e4c7cfdedc8be
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-06 08:30:32 -0300

    agregue la ejecución de codigo sobre la api servida por visualstudiocode

---
+++ src/sileg/src/environments/environment.vs.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: true,
+  silegApiUrl: 'http://localhost:9005/sileg/api/v1.0',
+  oidp_issuer: 'https://oidp.econo.unlp.edu.ar',
+  userinfoEndpoint: 'https://oidp.econo.unlp.edu.ar/userinfo',
+  loginUrl: 'https://oidp.econo.unlp.edu.ar/oauth2/auth',
+  logoutUrl: 'https://consent.econo.unlp.edu.ar/logout',
+  // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
+  loaderHeader: 'Gelis | FCE',
+  loaderLogo: '/assets/img/logofce2018.png',
+  loaderFooter1: 'DiTESI | Dirección de tecnologías y Sistemas Informáticos | Facultad de Ciencias Económicas | UNLP | Calle 6 Nº 777',
+  loaderFooter2: 'DiTESI | Dirección de tecnologías y Sistemas Informáticos | FCE',
+  loaderBackground: '/assets/img/fondo.jpg',
+
+  menuFooter: '  DiTeSI | Dirección de Tecnologías y Sistemas Informáticos | FCE',
+
+  pantallaPrincipalLogo: '/assets/img/logo_gelis_fce.png',
+};
