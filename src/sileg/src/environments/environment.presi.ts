6d7f3ca9f6b8bd2e2879552c48172c1a30772c21
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-09-13 12:29:34 -0300

    le actualice a miguel el environment

--- src/sileg/src/environments/environment.presi.ts
+++ src/sileg/src/environments/environment.presi.ts
@@ -1,11 +1,10 @@
 export const environment = {
   production: true,
   silegApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/sileg/api/v1.0',
+  usuarioApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/users/api/v1.0',
   oidp_issuer: 'https://oidp.asistencia.presi.unlp.edu.ar',
-  userinfoEndpoint: 'https://oidp.asistencia.presi.unlp.edu.ar/userinfo',
-  loginUrl: 'https://oidp.asistencia.presi.unlp.edu.ar/oauth2/auth',
-  logoutUrl: 'https://consent.asistencia.presi.unlp.edu.ar/logout',
-  // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
+  logoutUrl: 'https://login.asistencia.presi.unlp.edu.ar/logout/{{id_token}}/{{client_id}}',
+
   loaderHeader: 'Gelis | Presidencia',
   loaderLogo: '/assets/img/logounlp.png',
   loaderFooter1: 'Dirección General de Personal | Presidencia | UNLP | Calle 7 Nº 776',
