9799d95256664695934909417b0e0affe6e46957
Miguel Macagno <miguel.macagno@gmail.com>
2018-10-06 19:50:19 -0300

    Se agrego modo debug de sileg api y correspondiente environment

---
+++ src/sileg/src/environments/environment.vs.presi.ts
@@ -0,0 +1,26 @@
+export const environment = {
+  production: true,
+  silegApiUrl: 'http://localhost:10202/sileg/api/v1.0',
+  //usuarioApiUrl: 'http://localhost:10102/users/api/v1.0',
+  //loginApiUrl: 'http://localhost:10002/login/api/v1.0',
+
+  //silegApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/sileg/api/v1.0',
+  usuarioApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/users/api/v1.0',
+  loginApiUrl: 'https://api.asistencia.presi.unlp.edu.ar/login/api/v1.0',
+
+  oidp_issuer: 'https://oidp.asistencia.presi.unlp.edu.ar/',
+  logoutUrl: 'https://login.asistencia.presi.unlp.edu.ar/logout/{{id_token}}/{{client_id}}',
+
+  version: '0.1.1a',
+
+  loaderHeader: 'Gelis | Presidencia',
+  loaderLogo: '/assets/img/logounlp.png',
+  loaderFooter1: 'Dirección General de Personal | Presidencia | UNLP | Calle 7 Nº 776',
+  loaderFooter2: 'Dirección General de Personal | Presidencia | UNLP',
+  loaderBackground: '/assets/img/fondo_index_sileg_pre.jpg',
+
+  menuFooter: 'Dirección General de Personal | Presidencia | UNLP',
+
+  pantallaPrincipalLogo: '/assets/img/logo_gelis_unlp.png',
+  lugar: 'presi',
+};
