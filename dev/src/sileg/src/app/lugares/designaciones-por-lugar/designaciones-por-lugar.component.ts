8ece7c48aea5ae9488d5b466e0b2fd63d6218adb
Pablo Daniel Rey <pablo@econo.unlp.edu.ar>
2018-11-21 14:49:13 -0300

    agregue los cargos a la pantalla de designaciones

--- dev/src/sileg/src/app/lugares/designaciones-por-lugar/designaciones-por-lugar.component.ts
+++ dev/src/sileg/src/app/lugares/designaciones-por-lugar/designaciones-por-lugar.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MatTableDataSource, MatSort, Sort } from '@angular/material';
 
 import { SilegService } from '../../sileg.service';
-import { MatTableDataSource, MatSort, Sort } from '@angular/material';
+import { DatoDesignacion, Cargo } from '../../entities/sileg';
 
 @Component({
   selector: 'app-designaciones-por-lugar',
@@ -13,8 +14,10 @@ export class DesignacionesPorLugarComponent implements OnInit {
 
   cargando: boolean = false;
   lugar: any = null;
-  designaciones : MatTableDataSource<any> = null;
-  columnas: string[] = [];  
+  designaciones : MatTableDataSource<DatoDesignacion> = null;
+  columnas: string[] = ['fullname','dni','cargo'];  
+  cargos: any[] = [];
+  subscriptions: any[] = [];
 
   constructor(private route : ActivatedRoute,
               private service : SilegService) { 
@@ -22,12 +25,26 @@ export class DesignacionesPorLugarComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.queryParamMap.subscribe(p => {
+    this.obtenerTiposCargos();
+    this.route.paramMap.subscribe(p => {
       let lid = p.get('id');
       this.service.buscarLugar(lid).subscribe(l => {
         this.lugar = l;
+        this.service.obtenerDesignacionesLugares(lid).subscribe(ds => {
+          console.log(ds);
+          this.designaciones = new MatTableDataSource(ds.designaciones);
+        });
       });
     });
+  }
+
+  obtenerTiposCargos() {
+    this.cargos = [];
+    this.subscriptions.push(this.service.cargos()
+      .subscribe(r => {
+        this.cargos = r;
+        this.cargos.sort((c1:Cargo, c2:Cargo) => c1.nombre.localeCompare(c2.nombre));
+      }));
   }
 
 }
