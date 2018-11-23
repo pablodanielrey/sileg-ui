7dd5061021aa5847e73b4d77c23867a8366e3f5d
Pablo Daniel Rey <pablo@econo.unlp.edu.ar>
2018-11-23 15:05:49 -0300

    elimine algunas columnas porque son procesadas desde distintas fuentes. pueden variar

--- dev/src/sileg/src/app/designacion/designaciones-por-lugar/designaciones-por-lugar.component.ts
+++ dev/src/sileg/src/app/designacion/designaciones-por-lugar/designaciones-por-lugar.component.ts
@@ -15,7 +15,7 @@ export class DesignacionesPorLugarComponent implements OnInit {
   cargando: boolean = false;
   lugar: any = null;
   designaciones : MatTableDataSource<DatoDesignacion> = null;
-  columnas: string[] = ['fullname','dni','tipo_designacion','cargo','desde','hasta','expediente','resolucion','lugar','estado'];  
+  columnas: string[] = ['fullname','dni','tipo_designacion','cargo','desde','hasta','lugar','estado'];  
   cargos: Cargo[] = [];
   subscriptions: any[] = [];
 
